a0749b6defb3f3a79dbe6445e397bb1309f6ea4d
su <recallsufuture@gmail.com>
2020-01-10 13:47:51 +0800

    refactor: change directory structure

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from "./main/App.vue";
+import router from "./main/router";
+import store from "./main/store";
 
 Vue.config.productionTip = false;
 
