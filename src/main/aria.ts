76fca179abb331a68857534999ab16ce923e4dc6
su <recallsufuture@gmail.com>
2020-01-16 21:57:21 +0800

    feat: add aria2

---
+++ src/main/aria.ts
@@ -0,0 +1,93 @@
+import { app } from "electron";
+import is from "electron-is";
+import { existsSync } from "fs";
+import { resolve, join } from "path";
+import { exec, ChildProcess } from "child_process";
+import logger from "./logger";
+import ariaNames from "./config/aria";
+
+// aria2 启动实例
+let instance: ChildProcess | null = null;
+
+/**
+ * 获取 aria2 启动命令
+ */
+function getStartSh() {
+  const { platform } = process;
+  let basePath = resolve(app.getAppPath(), "..");
+
+  // 开发时使用根目录下的 aria2
+  if (is.dev()) {
+    basePath = resolve(__dirname, `../extra/${platform}`);
+  }
+
+  // 获取各平台对应的可执行文件名
+  const binName = (ariaNames as any)[platform];
+  if (!binName) {
+    logger.error("Aria2 does not support this platform");
+    throw new Error("Aria2 does not support this platform");
+  }
+
+  // 检测 aria2 是否存在
+  const binPath = join(basePath, `/aria2/${binName}`);
+  const binIsExist = existsSync(binPath);
+  if (!binIsExist) {
+    logger.error("Aria2 bin is not exist===>", binPath);
+    throw new Error("Aria2 is not exist");
+  }
+
+  // 获取配置路径和 session 路径
+  const confPath = join(basePath, "/aria2/aria2.conf");
+  const sessionPath = app.getPath("userData") + "/session";
+  const sessionIsExist = existsSync(sessionPath);
+
+  // 生成命令
+  let result = [
+    `${binPath}`,
+    `--conf-path=${confPath}`,
+    `--save-session=${sessionPath}`
+  ];
+  if (sessionIsExist) {
+    result = [...result, `--input-file=${sessionPath}`];
+  }
+
+  return result;
+}
+
+// 启动 aria2
+export function start() {
+  const sh = getStartSh();
+  logger.info("[Furaffinity-dl] Aria2 start sh===>", sh);
+  instance = exec(sh.join(" "), (err, stdout, stderr) => {
+    if (err) {
+      logger.info(`[Furaffinity-dl] Aria2 error===> ${err}`);
+      return;
+    }
+  });
+
+  instance.on("exit", (code, signal) => {
+    logger.info(`[Furaffinity-dl] Aria2 exit===> ${code}`);
+  });
+}
+
+// 关闭 aria2
+export function stop() {
+  try {
+    logger.info("[Furaffinity-dl] Aria2 stopping===>");
+    instance?.kill();
+  } catch (err) {
+    logger.error("[Furaffinity-dl] Aria2 stop fail===>", err.message);
+  }
+}
+
+// 重启 aria2
+export function restart() {
+  stop();
+  start();
+}
+
+export default {
+  start,
+  stop,
+  restart
+};
---
+++ vue.config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "Furaffinity-dl",
+        appId: "space.recallsufuture.furaffinity-dl",
+        asar: true,
+        publish: [
+          {
+            provider: "github",
+            owner: "recallfuture",
+            repo: "furaffinity-dl",
+            releaseType: "draft"
+          }
+        ],
+        // dmg: {
+        //   contents: [
+        //     {
+        //       x: 410,
+        //       y: 150,
+        //       type: "link",
+        //       path: "/Applications"
+        //     },
+        //     {
+        //       x: 130,
+        //       y: 150,
+        //       type: "file"
+        //     }
+        //   ]
+        // },
+        // mac: {
+        //   target: ["dmg", "zip"],
+        //   type: "distribution",
+        //   extraResources: {
+        //     from: "./extra/darwin/",
+        //     to: "./",
+        //     filter: ["**/*"]
+        //   },
+        //   binaries: [
+        //     "./release/mac/Furaffinity-dl.app/Contents/Resources/engine/aria2c"
+        //   ],
+        //   category: "public.app-category.utilities"
+        // },
+        win: {
+          target: [
+            {
+              target: "nsis",
+              arch: ["x64", "ia32"]
+            },
+            {
+              target: "zip",
+              arch: ["x64", "ia32"]
+            },
+            {
+              target: "portable",
+              arch: ["x64", "ia32"]
+            }
+          ],
+          extraResources: {
+            from: "./extra/win32/",
+            to: "./",
+            filter: ["**/*"]
+          }
+        },
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true
+        },
+        linux: {
+          category: "Network",
+          target: ["deb", "snap", "AppImage"],
+          extraResources: {
+            from: "./extra/linux/",
+            to: "./",
+            filter: ["**/*"]
+          }
+        }
+      }
+    }
+  }
+};
