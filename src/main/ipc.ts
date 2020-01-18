87d51c42446e9b41c931104df34efd936faafc16
su <recallsufuture@gmail.com>
2020-01-18 12:57:36 +0800

    improvement: add ipc

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,21 +1,43 @@
 import ipc from "electron-promise-ipc";
 import { session } from "electron";
-import { User } from "furaffinity-api";
+import {
+  User,
+  WatchingList,
+  Author,
+  Gallery,
+  Scraps,
+  Submission
+} from "furaffinity-api";
+import db from "./database";
 
-export function registerMainIpc() {
-  ipc.on("main.beforeLogin", () => {
-    // clear cookies
-    session.defaultSession?.cookies.remove(".furaffinity.net", "a");
-    session.defaultSession?.cookies.remove(".furaffinity.net", "b");
-  });
+function onFaClearCookies() {
+  session.defaultSession?.cookies.remove(".furaffinity.net", "a");
+  session.defaultSession?.cookies.remove(".furaffinity.net", "b");
+}
 
-  ipc.on("main.login", () => {
-    return session.defaultSession?.cookies.get({
-      url: ".furaffinity.net"
-    });
+function onFaGetCookies() {
+  return session.defaultSession?.cookies.get({
+    url: ".furaffinity.net"
   });
+}
 
-  ipc.on("main.user", () => {
-    return User();
-  });
+export function registerFaIpc() {
+  // cookies
+  ipc.on("fa.clearCookies", onFaClearCookies);
+  ipc.on("fa.getCookies", onFaGetCookies);
+
+  // fa api
+  ipc.on("fa.user", User);
+  ipc.on("fa.watchingList", (id: any) => WatchingList(id));
+  ipc.on("fa.author", (id: any) => Author(id));
+  ipc.on("fa.gallery", (id: any, page: any) => Gallery(id, page));
+  ipc.on("fa.scraps", (id: any, page: any) => Scraps(id, page));
+  ipc.on("fa.submission", (id: any) => Submission(id));
+}
+
+export function registerDbIpc() {
+  ipc.on("db.getUserConfig", db.userConfig.get);
+  ipc.on("db.setUserConfig", (data: any) => db.userConfig.set(data));
+  ipc.on("db.getAriaConfig", db.ariaConfig.get);
+  ipc.on("db.setAriaConfig", (data: any) => db.ariaConfig.set(data));
 }
