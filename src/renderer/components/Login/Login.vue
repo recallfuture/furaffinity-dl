65763f048f4ab11bb86e3d4a7a97569118cb16de
su <recallsufuture@gmail.com>
2020-01-20 22:49:46 +0800

    improvement: add component Login

---
+++ src/renderer/components/Login/Login.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+v-container( fluid )
+  iframe( src="https://www.furaffinity.net/login/?mode=imagecaptcha" width="100%" height="500" )
+</template>
+
+<script>
+import { clearCookies, getCookies } from "@/renderer/api";
+
+export default {
+  name: "Login",
+
+  data() {
+    return {
+      timer: null,
+      success: false
+    };
+  },
+
+  async mounted() {
+    await clearCookies();
+    this.doCheck();
+  },
+
+  beforeDestroy() {
+    this.endCheck();
+  },
+
+  methods: {
+    async checkLogin() {
+      let a, b;
+      const cookies = await getCookies();
+      console.log(cookies);
+
+      cookies.forEach((cookie, index) => {
+        if (cookie.name === "a") {
+          a = cookie.value;
+        } else if (cookie.name === "b") {
+          b = cookie.value;
+        }
+      });
+
+      if (a && b) {
+        this.success = true;
+        this.$emit("success", { a, b });
+      }
+    },
+
+    doCheck() {
+      this.timer = setTimeout(async () => {
+        await this.checkLogin();
+        if (!this.success) {
+          this.doCheck();
+        }
+      }, 1000);
+    },
+
+    endCheck() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+        this.timer = null;
+      }
+    }
+  }
+};
+</script>
