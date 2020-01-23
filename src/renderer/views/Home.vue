d4682f0d7fdd3d3335372411d7fbead805140488
su <recallsufuture@gmail.com>
2020-01-23 17:06:43 +0800

    feat: add home

--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -1,11 +1,37 @@
-<template>
-  <div class="home"></div>
+<template lang="pug">
+v-app
+  //- 全局使用的组件
+  guide
+  add-subscription-dialog
+
+  //- 订阅抽屉栏
+  Drawer
+
+  //- 订阅详情控制栏
+  app-bar
+  v-content( class="grey lighten-5" )
 </template>
 
 <script>
 // @ is an alias to /src
+import Guide from "@/renderer/components/Guide/Guide";
+import AddSubscriptionDialog from "@/renderer/components/Subscription/AddSubscriptionDialog";
+import AppBar from "@/renderer/components/Main/AppBar";
+import Drawer from "@/renderer/components/Main/Drawer";
+import { app } from "electron";
 
 export default {
-  name: "home"
+  name: "home",
+
+  data() {
+    return {};
+  },
+
+  components: {
+    Guide,
+    AddSubscriptionDialog,
+    AppBar,
+    Drawer
+  }
 };
 </script>
