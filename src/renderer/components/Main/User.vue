80415423f2a61e0f8e7ed2b58d8d658009049b2a
su <recallsufuture@gmail.com>
2020-01-23 17:23:45 +0800

    improvement: add component User

---
+++ src/renderer/components/Main/User.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  //- 用户信息不为空就展示出来
+  div( v-if="user" )
+    v-avatar( size="36" class="mx-2" )
+      img( :src="user.avatar" )
+    span( class="mr-2" ) {{ user.name }}
+
+  //- 为空就显示登录按钮
+  div( v-else )
+    v-btn( @click="dialog = true" text ) 登录
+    v-dialog( v-model="dialog" width="800" )
+      v-card( color="white" )
+        //- 隐藏登录框同时销毁组件
+        login( v-if="dialog" @success="loginSuccess" )
+</template>
+
+<script>
+import Login from "../Login/Login";
+
+export default {
+  name: "User",
+
+  model: {
+    prop: "user",
+    event: "change"
+  },
+
+  props: {
+    user: {
+      type: Object,
+      default: null
+    }
+  },
+
+  components: {
+    Login
+  },
+
+  data() {
+    return {
+      dialog: false
+    };
+  },
+
+  methods: {
+    loginSuccess(user) {
+      this.$emit("change", user);
+      this.dialog = false;
+    }
+  }
+};
+</script>
