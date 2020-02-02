ea46543a2df14d40c923a68bfc67e4d6dd833dde
su <recallsufuture@gmail.com>
2020-02-02 21:09:30 +0800

    improvement: 允许修改批量添加订阅时的文件夹位置

--- src/renderer/components/Subscription/AddSubFromWatchingList.vue
+++ src/renderer/components/Subscription/AddSubFromWatchingList.vue
@@ -5,7 +5,17 @@
       v-row
         v-col( cols="12" )
           dir-field( v-model="dir" label="订阅下载根目录" )
-          span( class="grey--text" ) 将下载到此目录下对应的的作者名文件夹中
+        v-col( cols="3" )
+          v-container( fill-height )
+            span 订阅文件夹名格式：
+        v-col( cols="3" )
+          v-text-field( v-model="prefix" label="前缀" )
+        v-col( cols="3" )
+          v-select( v-model="dirName" :items="dirItems" label="订阅文件夹名" )
+        v-col( cols="3" )
+          v-text-field( v-model="suffix" label="后缀" )
+        v-col( cols="12" )
+          span 例如，一个用户名为Furry，id为furry的订阅，将存储到 {{ getPath({id: "furry", name: "Furry"}) }}
       
       //- Gallery 和 Scraps 选项
       v-row
@@ -21,6 +31,7 @@
 <script>
 import path from "path";
 import fs from "fs";
+import _ from "lodash";
 import { convertNameToId } from "@/shared/utils";
 import { faWatchingList } from "@/renderer/api";
 import bus from "@/renderer/utils/EventBus";
@@ -46,7 +57,18 @@ export default {
 
       dir: this.config.dir,
       gallery: true,
-      scraps: false
+      scraps: false,
+
+      advanced: false,
+      prefix: "",
+      dirName: "作者id",
+      dirItems: [
+        { text: "作者id" },
+        { text: "作者名" },
+        { text: "作者id，首字母大写" },
+        { text: "作者名，首字母大写" }
+      ],
+      suffix: ""
     };
   },
 
@@ -71,6 +93,32 @@ export default {
       }
     },
 
+    getPath(user) {
+      let baseName;
+      switch (this.dirName) {
+        case "作者名": {
+          baseName = this.prefix.trim() + user.name + this.suffix.trim();
+          break;
+        }
+        case "作者id，首字母大写": {
+          baseName =
+            this.prefix.trim() + _.upperFirst(user.id) + this.suffix.trim();
+          break;
+        }
+        case "作者名，首字母大写": {
+          baseName =
+            this.prefix.trim() + _.upperFirst(user.name) + this.suffix.trim();
+          break;
+        }
+        default: {
+          // 作者id
+          baseName = this.prefix.trim() + user.id + this.suffix.trim();
+          break;
+        }
+      }
+      return path.join(this.dir, baseName);
+    },
+
     // 验证其他选项
     async doFetch() {
       if (!this.form) {
@@ -81,7 +129,7 @@ export default {
       const users = await faWatchingList();
 
       const subs = users.map(user => {
-        const baseDir = path.join(this.dir, user.id);
+        const baseDir = this.getPath(user);
         const galleryDir = baseDir;
         const scrapsDir = path.join(baseDir, "scraps");
 
