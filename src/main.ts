1317377d1fab68ff00b0573a0dacaf739565875d
SU <recallsufuture@gmail.com>
2020-02-04 04:04:30 +0800

    fix: 全局错误捕获

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ import "@mdi/font/css/materialdesignicons.css";
 
 // vue2 animate
 import "vue2-animate/dist/vue2-animate.min.css";
+import logger from "@/shared/logger";
 
 Vue.config.productionTip = false;
+Vue.config.errorHandler = (error, vm) => {
+  logger.error("Vue error", error, vm);
+};
 
 // @ts-ignore
 new Vue({
