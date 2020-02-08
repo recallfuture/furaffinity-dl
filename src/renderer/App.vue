0e81173fd2717447fa0bccece3994b40cb9d9f91
su <recallsufuture@gmail.com>
2020-02-08 14:08:56 +0800

    improvement: 分离顶部工具栏组件

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,24 +1,7 @@
 <template lang="pug">
   el-container( style="height: 100vh;" )
     el-header
-      el-button( type="primary" icon="el-icon-plus" ) {{ $t("header.add") }}
-
-      el-button-group( style="margin-left: 10px; margin-right: 10px;" )
-        el-button( type="info" icon="el-icon-video-play" ) {{ $t("header.start") }}
-        el-button( type="info" icon="el-icon-video-pause" disabled ) {{ $t("header.stop") }}
-        el-button( type="info" icon="el-icon-delete" ) {{ $t("header.delete") }}
-      
-      div( class="spacer" )
-
-      el-dropdown
-        div( class="user" )
-          el-avatar( v-if="user" class="avatar" :size="36" :src="user.avatar" )
-          span( v-if="user" ) {{ user.name }}
-          i( class="el-icon-caret-bottom" )
-        el-dropdown-menu( slot="dropdown" )
-          el-dropdown-item {{ $t("header.logout") }}
-      
-      el-button( type="info" icon="el-icon-setting" ) {{ $t("header.setting") }}
+      Toolbar( :user="user" )
 
     el-main
       sub-table()
@@ -30,13 +13,16 @@
 <script lang="ts">
 import { Vue, Component, Prop, ProvideReactive } from "vue-property-decorator";
 import { Subscription } from "@/main/database/entity";
-import SubTable from "./components/main/SubTable.vue";
 import { getSubs } from "./api/database";
 import logger from "@/shared/logger";
 import { Author } from "furaffinity-api/dist/interfaces";
 
+// 组件
+import Toolbar from "./components/header/Toolbar.vue";
+import SubTable from "./components/main/SubTable.vue";
+
 @Component({
-  components: { SubTable }
+  components: { Toolbar, SubTable }
 })
 export default class App extends Vue {
   @ProvideReactive() subs: Subscription[] = [];
@@ -59,22 +45,6 @@ body {
   margin: 0;
 }
 
-.spacer {
-  flex-grow: 1;
-}
-
-.user {
-  color: rgba(256, 256, 256, 0.87);
-  display: flex;
-  align-items: center;
-  margin-right: 10px;
-  font-size: 1.1rem;
-}
-
-.user .avatar {
-  margin-right: 5px;
-}
-
 .el-scrollbar__wrap {
   overflow-x: hidden !important;
 }
@@ -82,8 +52,6 @@ body {
 .el-header {
   background-color: #121212;
   color: rgba(256, 256, 256, 0.87);
-  display: flex;
-  align-items: center;
 }
 
 .el-main {
---
+++ src/renderer/components/header/Toolbar.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+  div( class="toolbar" )
+    //- 添加订阅
+    el-button( type="primary" icon="el-icon-plus" ) {{ $t("header.add") }}
+
+    //- 订阅下载控制
+    el-button-group( style="margin-left: 10px; margin-right: 10px;" )
+      el-button( type="info" icon="el-icon-video-play" ) {{ $t("header.start") }}
+      el-button( type="info" icon="el-icon-video-pause" disabled ) {{ $t("header.stop") }}
+      el-button( type="info" icon="el-icon-delete" ) {{ $t("header.delete") }}
+    
+    div( class="spacer" )
+
+    //- 用户登录注销
+    el-dropdown( v-if="user" )
+      div( class="user" )
+        el-avatar( class="avatar" :size="36" :src="user.avatar" )
+        span {{ user.name }}
+        i( class="el-icon-caret-bottom" )
+      
+      el-dropdown-menu( slot="dropdown" )
+        el-dropdown-item {{ $t("header.logout") }}
+    el-button( v-else type="primary" icon="el-icon-user" ) 登录FA
+    
+    //- 设置
+    el-button( type="info" icon="el-icon-setting" ) {{ $t("header.setting") }}
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, ProvideReactive } from "vue-property-decorator";
+import { Author } from "furaffinity-api/dist/interfaces";
+
+@Component
+export default class Toolbar extends Vue {
+  @Prop(Object) user!: Author;
+}
+</script>
+
+<style>
+.toolbar {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.spacer {
+  flex-grow: 1;
+}
+
+.user {
+  color: rgba(256, 256, 256, 0.87);
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+  font-size: 1.1rem;
+}
+
+.user .avatar {
+  margin-right: 5px;
+}
+</style>
