0b92dbb053c725cda34ffccf01ab5b2377d63286
su <recallsufuture@gmail.com>
2020-05-24 22:58:00 +0800

    release: 0.2.5

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -2,5 +2,6 @@ import logger from "electron-log";
 import is from "electron-is";
 
 logger.transports.console.level = is.production() ? "warn" : "silly";
+logger.transports.file.level = is.production() ? "debug" : false;
 
 export default logger;
--- vue.config.js
+++ vue.config.js
@@ -24,55 +24,15 @@ module.exports = {
         productName: "Furaffinity-dl",
         appId: "space.recallsufuture.furaffinity-dl",
         asar: true,
-        publish: [
-          {
-            provider: "github",
-            owner: "recallfuture",
-            repo: "furaffinity-dl",
-            releaseType: "draft"
-          }
-        ],
-        dmg: {
-          contents: [
-            {
-              x: 410,
-              y: 150,
-              type: "link",
-              path: "/Applications"
-            },
-            {
-              x: 130,
-              y: 150,
-              type: "file"
-            }
-          ]
-        },
-        mac: {
-          target: ["dmg", "zip"],
-          type: "distribution",
-          extraResources: {
-            from: "./extra/darwin/",
-            to: "./",
-            filter: ["**/*"]
-          },
-          binaries: [
-            "./release/mac/Furaffinity-dl.app/Contents/Resources/engine/aria2c"
-          ],
-          category: "public.app-category.utilities"
-        },
         win: {
           target: [
-            {
-              target: "nsis",
-              arch: ["x64", "ia32"]
-            },
+            // {
+            //   target: "nsis",
+            //   arch: ["x64"]
+            // },
             {
               target: "zip",
-              arch: ["x64", "ia32"]
-            },
-            {
-              target: "portable",
-              arch: ["x64", "ia32"]
+              arch: ["x64"]
             }
           ],
           extraResources: {
@@ -84,15 +44,6 @@ module.exports = {
         nsis: {
           oneClick: false,
           allowToChangeInstallationDirectory: true
-        },
-        linux: {
-          category: "Network",
-          target: ["deb", "snap", "AppImage"],
-          extraResources: {
-            from: "./extra/linux/",
-            to: "./",
-            filter: ["**/*"]
-          }
         }
       }
     }
